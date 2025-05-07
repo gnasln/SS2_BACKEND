d484a152bd84ddec0d9685656944bef63f24632f
gnasln <lnsangprod24@gmail.com>
2025-05-08 00:15:48 +0700

    create positionResponse

---
+++ Base_BE.Application/Dtos/PositionResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Base_BE.Application.Dtos;
+
+public class PositionResponse
+{
+    public Guid Id { get; set; }
+    public string? PositionName { get; set; }
+    public string? PositionDescription { get; set; } = string.Empty;
+    public bool? Status { get; set; } = false;
+}
