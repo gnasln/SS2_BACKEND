ca4827db6e3b4738e5d31962fc672da02273fe6c
gnasln <lnsangprod24@gmail.com>
2025-05-15 11:46:38 +0700

    add candidateDTO

---
+++ Base_BE.Application/Dtos/CandidateDto.cs
@@ -0,0 +1,15 @@
+namespace Base_BE.Application.Dtos;
+public class CandidateDto
+{
+    public required string Id { get; set; }
+    public string? FullName { get; set; }
+    public string? UserName { get; set; }
+    public string? Email { get; set; }
+    public string? NewEmail { get; set; }
+    public string? Address { get; set; }
+    public string? CellPhone { get; set; }
+    public DateTime? Birthday { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? IdentityCardImage { get; set; }
+    public int? TotalBallot { get; set; } = 0;
+}
