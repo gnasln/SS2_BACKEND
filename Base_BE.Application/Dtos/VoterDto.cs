26858f9a46aa04c956f1702aec51f37b40eeec5a
gnasln <lnsangprod24@gmail.com>
2025-05-15 11:44:43 +0700

    add voteDto

---
+++ Base_BE.Application/Dtos/VoterDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_BE.Application.Dtos
+{
+    public class VoterDto
+    {
+        public string? Id { get; set; }
+        public string? Fullname { get; set; }
+        public string? Email { get; set; }
+        public string? NewEmail { get; set; }
+        public string? Address { get; set; }
+        public string? CellPhone { get; set; }
+        public DateTime? Birthday { get; set; }
+        public string? ImageUrl { get; set; }
+        public bool? Status { get; set; }
+    }
+}
